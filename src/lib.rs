ac9da8beb1f63ab7459ef9d80f70add2588dd65f
Shelby Doolittle <shelby@shelbyd.com>
2024-12-19 01:16:45 -0500

    add algorithm to documentation

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,12 @@
 //! Faro Sharding shows high quality even distribution among locations. With 1,000,000 keys and 100 locations, the largest has 10332 keys (1.03%) and the smallest has 9854 keys (0.99%).
 //!
 //! If you want to evaluate the distribution with your own hashing function, you can modify `examples/distribution.rs` to use your hasher.
+//!
+//! # Algorithm
+//!
+//! Inspired by [JumpHash](https://arxiv.org/abs/1406.2294), Faro Sharding sequentially hashes the initial key and then the results of those hashes. Different from JumpHash, Faro Sharding only changes the shard for a key when `hash % i == 0`.
+//!
+//! Faro Sharding can only move a key to a shard on the step corresponding to that shard number. That means that the only movement happens to shard N on step N. Additionally, approximately 1/N of keys are moved on step N, resulting in approximately even distribution.
 
 use std::hash::*;
 
